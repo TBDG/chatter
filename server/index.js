b1bff5ed625a3caaa3baba5198f120592a5a1105
AvocadoCasualty <baibeebambi@gmail.com>
2020-10-05 15:25:41 -0700

    Preliminary server setup

--- server/index.js
+++ server/index.js
@@ -1,2 +1,27 @@
-console.log('server started!!');
+require('dotenv').config()
+const express = require('express');
+const socket = require('socket.io')
+const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING} = process.env
 
+//App set up
+const app = express();
+
+const server = app.listen(4000, () =>{
+    console.log(`Listening for requests on port ${SERVER_PORT}`)
+})
+
+//socket setup
+const io = socket(server);
+
+io.on('connection', (socket)=>{
+    console.log(`Made socket connection ${socket.id}`)
+
+    //handle chat event
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+})
